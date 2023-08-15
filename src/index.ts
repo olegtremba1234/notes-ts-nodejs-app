ac76597b849977265eb07a9926bc8ac9a09a7e27
olegtremba1234 <olegtremba@gmail.com>
2023-08-15 23:52:56 +0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,38 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import notesRoutes from './routes/notesRoutes';
-import globalErrorHandler from './middlewares/globalHandleError';
-import swaggerUi from 'swagger-ui-express';
-const swaggerDocument = require('../swagger.json');
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import notesRoutes from "./routes/notesRoutes";
+import globalErrorHandler from "./middlewares/globalHandleError";
+import swaggerUi from "swagger-ui-express";
+const swaggerDocument = require("../swagger.json");
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const { PORT, MONGODB_URL } = process.env
+const { PORT, MONGODB_URL } = process.env;
 
 mongoose
-.connect(MONGODB_URL!)
+  .connect(MONGODB_URL!)
   .then(() => {
-  console.log("Database connection successful");
-  app.use(bodyParser.json());
-    
-  // Routes
-  app.use('/api', notesRoutes);
-    
-    app.use(globalErrorHandler)
-    
-    app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    
-  // Start the server
-  app.listen(PORT || 3001, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log("Database connection successful");
+    app.use(cors());
+    app.use(bodyParser.json());
+
+    // Routes
+    app.use("/api", notesRoutes);
+
+    app.use(globalErrorHandler);
+
+    app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+    // Start the server
+    app.listen(PORT || 3001, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error.message);
+    process.exit(1);
   });
-})
-.catch((error) => {
-  console.error('Failed to connect to the database:', error.message);
-  process.exit(1);
-});
