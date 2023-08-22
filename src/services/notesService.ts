0faaca8890fcd4f0d5446e9a215bd0052d692cb4
olegtremba1234 <olegtremba@gmail.com>
2023-08-22 12:35:46 +0300

    add sort by date in mongodb

--- src/services/notesService.ts
+++ src/services/notesService.ts
@@ -1,12 +1,11 @@
-import { updateNoteById } from './../controllers/notesController';
-import mongoose from 'mongoose';
-import NoteModel, { Note, NoteCategory } from '../models/noteModel';
-import { validateNote, validateId } from '../helpers/validation';
-import dateParser from '../helpers/dateParser';
+import mongoose from "mongoose";
+import NoteModel, { Note, NoteCategory } from "../models/noteModel";
+import { validateNote, validateId } from "../helpers/validation";
+import dateParser from "../helpers/dateParser";
 
 class NotesService {
   async getAllNotes(): Promise<Note[]> {
-    return NoteModel.find().exec();
+    return await NoteModel.find().sort({ createdAt: -1 });
   }
 
   async getNoteById(id: string): Promise<Note | null> {
@@ -15,7 +14,7 @@ class NotesService {
       const note = await NoteModel.findById(noteId);
       return note;
     } catch (error) {
-      console.error('Error getting note by ID:', error);
+      console.error("Error getting note by ID:", error);
       return null;
     }
   }
@@ -30,48 +29,54 @@ class NotesService {
     await validateId(id);
     await validateNote(updatedNote);
     const datesMentioned = dateParser(updatedNote.content);
-  try {
-    const noteId = new mongoose.Types.ObjectId(id);
-    const updateNoteWithDates = {...updatedNote, datesMentioned}
-    return NoteModel.findByIdAndUpdate(noteId, updateNoteWithDates, { new: true }).exec();
-  } catch (error) {
-    console.error('Error updating note by ID:', error);
-    return null;
+    try {
+      const noteId = new mongoose.Types.ObjectId(id);
+      const updateNoteWithDates = { ...updatedNote, datesMentioned };
+      return NoteModel.findByIdAndUpdate(noteId, updateNoteWithDates, {
+        new: true,
+      }).exec();
+    } catch (error) {
+      console.error("Error updating note by ID:", error);
+      return null;
+    }
   }
-}
 
   async deleteNoteById(id: string): Promise<Note | null> {
     const noteId = new mongoose.Types.ObjectId(id);
     return await NoteModel.findOneAndDelete(noteId).exec();
   }
 
-  async getAggregatedStats(): Promise<{ [category: string]: {totalActive: number; totalArchived: number } }> {
-  try {
-    const aggregatedStats = await NoteModel.aggregate([
-      {
-        $group: {
-          _id: '$category',
-          totalActive: { $sum: { $cond: ['$archived', 0, 1] } },
-          totalArchived: { $sum: { $cond: ['$archived', 1, 0] } },
+  async getAggregatedStats(): Promise<{
+    [category: string]: { totalActive: number; totalArchived: number };
+  }> {
+    try {
+      const aggregatedStats = await NoteModel.aggregate([
+        {
+          $group: {
+            _id: "$category",
+            totalActive: { $sum: { $cond: ["$archived", 0, 1] } },
+            totalArchived: { $sum: { $cond: ["$archived", 1, 0] } },
+          },
         },
-      },
-    ]).exec();
+      ]).exec();
 
-    const statsByCategory: { [category: string]: {totalActive: number; totalArchived: number } } = {};
-    aggregatedStats.forEach((categoryStats: any) => {
-      const category = categoryStats._id as NoteCategory;
-      statsByCategory[category] = {
-        totalActive: categoryStats.totalActive,
-        totalArchived: categoryStats.totalArchived,
-      };
-    });
+      const statsByCategory: {
+        [category: string]: { totalActive: number; totalArchived: number };
+      } = {};
+      aggregatedStats.forEach((categoryStats: any) => {
+        const category = categoryStats._id as NoteCategory;
+        statsByCategory[category] = {
+          totalActive: categoryStats.totalActive,
+          totalArchived: categoryStats.totalArchived,
+        };
+      });
 
-    return statsByCategory;
-  } catch (error) {
-    console.error('Error getting aggregated stats:', error);
-    return {};
+      return statsByCategory;
+    } catch (error) {
+      console.error("Error getting aggregated stats:", error);
+      return {};
+    }
   }
-}
 }
 
 export const notesService = new NotesService();
